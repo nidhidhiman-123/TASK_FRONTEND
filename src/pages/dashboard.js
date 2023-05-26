231e0ee6fde115a1a7fcb99606891f58228388ca
Nidhi Dhiman <104757239+nidhidhiman-123@users.noreply.github.com>
2023-05-26 18:17:09 +0530

    Add files via upload

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -7,23 +6,57 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import { BASE_URL } from "../baseUrl";
 
+  
 export default function Dashboard() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [uploadProgress, setUploadProgress] = useState(0);
   const [link, setLink] = useState(false);
   const navigate = useNavigate();
+
   useEffect(() => {
     let authTokens = localStorage.getItem("authToken");
     if (!authTokens) {
-      navigate("/login"); 
+      navigate("/login");
     } else {
       setLink(true);
     }
-  });
+  }, [navigate]);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
   const logout = () => {
     localStorage.removeItem("authToken");
-
     navigate("/login");
   };
+
+  const handleUpload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append('video', selectedFile);
+
+      const response = await axios.post(`${BASE_URL}/video`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        onUploadProgress: (progressEvent) => {
+          const progress = Math.round((progressEvent.loaded / progressEvent.total) * 100);
+          setUploadProgress(progress);
+        },
+      });
+
+      console.log(response.data);
+
+    } catch (error) {
+      console.error('Error:', error);
+
+    }
+  };
+
   return (
     <>
       {link ? (
@@ -37,7 +70,6 @@ export default function Dashboard() {
                 aria-label="menu"
                 sx={{ mr: 2 }}
               >
-
               </IconButton>
               <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                 Dashboard
@@ -50,11 +82,10 @@ export default function Dashboard() {
         ""
       )}
       <div>
-        <h1>Welcome To Dashboard Page</h1>
-
-
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={handleUpload}>Upload Video</button>
+        <div>{uploadProgress}% uploaded</div>
       </div>
     </>
-
   );
 }
